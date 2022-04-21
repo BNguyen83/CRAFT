1b6a314ad357963129fece67faff0818680b893d
jcalimbahin <92069001+jcalimbahin@users.noreply.github.com>
2022-04-20 21:09:07 -0700

    Update main_ui.ino

--- main_ui.ino
+++ main_ui.ino
@@ -1,5 +1,10 @@
 #include <LiquidCrystal.h>
 #include <Keypad.h>
+#include <SPI.h> // SD card headers
+#include <SD.h>
+#include <String.h>
+
+File myFile;
 
 // LCD Pins
 uint8_t DB7 = 39; // Pins for 4-bit operation
@@ -54,12 +59,18 @@ float maxRemForce; // Removal force
 float maxInSpeed;  // Insertion speed 
 float dwellTime;
 
+// State machine flags
 //Main
 int mainState = 0;
 // Menu
 int menuState = 0;
 // Test
 int testState = 0;
+int printFlag = 0;
+  int    i = 0;
+  
+  char arr[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
 
 void setup() 
 { 
@@ -67,17 +78,21 @@ void setup()
   top.begin(40,2);
   bot.begin(40,2);
 
+  Serial.begin(9600);
+
   /* Other function initializations go here */
 }
 
 void loop() 
 {
-  char key = myKeypad.getKey();
+  char   key = myKeypad.getKey();
+
   
   switch(mainState){
-    case 0: // Start Menu
+    case 0: /*** Main menu ***/
       switch(menuState){ // Sub-state
         case 0: // Main menu prompt
+        if (printFlag == 0){
           bot.clear();
           top.setCursor(1,0);
           top.print("Welcome to C.R.A.F.T.!");
@@ -85,14 +100,19 @@ void loop()
           top.print("Connection Resistance And Force Tester ");
 
           bot.setCursor(1,1);
-          bot.print("<A> Begin test  <B> Test Cycle ");
-
+          bot.print("<A> Begin test  <B> Test Cycle ");}
+          printFlag = 1;
           if(key == 'A'){
-            menuState++; // Menu state = 1, does it go to case 1 now?
+            menuState++;
+            printFlag = 0;
+            // Menu state = 1, does it go to case 1 now?
           } else if(key == 'B'){
-            mainState += 2;
-          }break;
+            //mainState += 2;
+            printFlag = 0;
+          }
+          break;
         case 1: // Cycle count
+        if(printFlag == 0){
           top.clear();
           bot.clear();
           bot.setCursor(21,1);
@@ -101,15 +121,31 @@ void loop()
           top.print("Set test parameters...");
     
           bot.setCursor(1,0); 
-          bot.print("Cycle count: ");
-          /* Code where keypad is scanned */
+          bot.print("Cycle count: ");}
+          printFlag = 1;
+
+          if(key > 47 && key < 58 && i < 9){
+            bot.setCursor(14+i,0);
+            arr[i] = myKeypad.getKey();
+            bot.print(key);
+            i++;
+          }
 
           if(key == '#'){
+            String ms = String(arr);
+            ms.remove(i+1);
+            cycleCount = ms.toInt(); 
+            Serial.println(cycleCount);
             menuState++;
+            i = 0;
+            printFlag = 0;
           } else if(key == '*'){
             menuState--;
+            printFlag = 0;
+            i = 0;
           }break;
         case 2: // Travel speed
+        if(printFlag == 0){
           top.clear();
           bot.clear();
           bot.setCursor(21,1);
@@ -118,15 +154,26 @@ void loop()
           top.print("Set test parameters...");
     
           bot.setCursor(1,0); 
-          bot.print("Travel speed: ");
-          /* Code where keypad is scanned */
+          bot.print("Travel speed: ");}
+          printFlag = 1;
+
+          if(key > 47 && key < 58 && i < 9){
+            bot.setCursor(15+i,0);
+            arr[i] = myKeypad.getKey();
+            bot.print(key);
+            i++;
+          }
 
           if(key == '#'){
+                      Serial.println(cycleCount);
             menuState++;
+            printFlag = 0;
           } else if(key == '*'){
             menuState--;
+            printFlag = 0;
           }break;
         case 3: // Travel distance
+        if(printFlag == 0){
           top.clear();
           bot.clear();
           bot.setCursor(21,1);
@@ -135,15 +182,18 @@ void loop()
           top.print("Set test parameters...");
     
           bot.setCursor(1,0); 
-          bot.print("Travel distance: ");
+          bot.print("Travel distance: ");}
           /* Code where keypad is scanned */
-
+          printFlag = 1;
           if(key == '#'){
             menuState++;
+            printFlag = 0;
           } else if(key == '*'){
             menuState--;
+            printFlag = 0;
           }break;
         case 4: // Max resistance
+        if(printFlag == 0){
           top.clear();
           bot.clear();
           bot.setCursor(21,1);
@@ -152,15 +202,18 @@ void loop()
           top.print("Set test parameters...");
     
           bot.setCursor(1,0); 
-          bot.print("Resistance (mOhms): ");
+          bot.print("Resistance (mOhms): ");}
           /* Code where keypad is scanned */
-
+          printFlag = 1;
           if(key == '#'){
             menuState++;
+            printFlag = 0;
           } else if(key == '*'){
             menuState--;
+            printFlag = 0;
           }break;
         case 5: // Max insertion force
+        if(printFlag == 0){
           top.clear();
           bot.clear();
           bot.setCursor(21,1);
@@ -169,15 +222,19 @@ void loop()
           top.print("Set test parameters...");
     
           bot.setCursor(1,0); 
-          bot.print("Insertion force (kg): ");
+          bot.print("Insertion force (kg): ");}
+          printFlag = 1;
           /* Code where keypad is scanned */
 
           if(key == '#'){
             menuState++;
+            printFlag = 0;
           } else if(key == '*'){
             menuState--;
+            printFlag = 0;
           }break;
         case 6: // Max removal force
+        if(printFlag == 0){
           top.clear();
           bot.clear();
           bot.setCursor(21,1);
@@ -186,15 +243,18 @@ void loop()
           top.print("Set test parameters...");
     
           bot.setCursor(1,0); 
-          bot.print("Removal force (kg): ");
+          bot.print("Removal force (kg): ");}
           /* Code where keypad is scanned */
-
+          printFlag = 1;
           if(key == '#'){
             menuState++;
+            printFlag = 0;
           } else if(key == '*'){
             menuState--;
+            printFlag = 0;
           }break;
         case 7: // Insertion speed
+        if(printFlag == 0){
           top.clear();
           bot.clear();
           bot.setCursor(21,1);
@@ -203,15 +263,18 @@ void loop()
           top.print("Set test parameters...");
     
           bot.setCursor(1,0); 
-          bot.print("Insertion speed: ");
+          bot.print("Insertion speed: ");}
           /* Code where keypad is scanned */
-
+          printFlag = 1;
           if(key == '#'){
             menuState++;
+            printFlag = 0;
           } else if(key == '*'){
             menuState--;
+            printFlag = 0;
           }break;
         case 8: // Dwell time
+        if(printFlag == 0){
           top.clear();
           bot.clear();
           bot.setCursor(21,1);
@@ -220,53 +283,74 @@ void loop()
           top.print("Set test parameters...");
     
           bot.setCursor(1,0); 
-          bot.print("Dwell time: ");
+          bot.print("Dwell time: ");}
+          printFlag = 1;
           /* Code where keypad is scanned */
 
           if(key == '#'){
             menuState++;
+            printFlag = 0;
           } else if(key == '*'){
             menuState--;
+            printFlag = 0;
           }break;
         case 9: // Confirm test
+        if(printFlag == 0){
           top.clear();
           bot.clear();
           bot.setCursor(24,1);
           bot.print("<*> No  <#> Yes");
           top.setCursor(1,0);
-          top.print("Begin test?");
+          top.print("Begin test?");}
+          printFlag = 1;
 
           if(key == '#'){
             mainState += 1;
+            printFlag = 0;
           } else if(key == '*'){
             menuState--;
+            printFlag = 0;
           }break;
         }break;
-    case 1: // Testing
-      top.clear();
-      bot.clear();
-      bot.setCursor(31,1);
-      bot.print("<#> Next");
-      top.setCursor(1,0);
-      top.print("Beginning test...");
-      bot.setCursor(1,0);
-      bot.print("Insert SD card now");
+        
+    case 1: /*** Testing ***/
+      switch(testState){
+        case 0: // Check for SD card
+        if(printFlag == 0){
+          top.clear();
+          bot.clear();
+          bot.setCursor(31,1);
+          bot.print("<#> Next");
+          top.setCursor(1,0);
+          top.print("Beginning test...");
+          bot.setCursor(1,0);
+          bot.print("Insert SD card now");}
+          printFlag = 1;
+          if(key == '#'){
+            printFlag = 0;
+            if(!SD.begin(4)){
+              /* If it returns true it initialized */
+              /* If it didn't, testState = 0 */
+            }
+            myFile = SD.open("test.txt", FILE_WRITE);
+            //myFile.println("testing 1, 2, 3.");
 
-      if(key == '#'){
-        /* Testing actually occurs here*/
-      }break;
-     
-    case 2: // Test cycle
-      top.clear();
-      bot.clear();
-      bot.setCursor(24,1);
-      bot.print("<*> No  <#> Yes");
-      top.setCursor(1,0);
-      top.print("Begin test cycle?");
+            testState++;
+          }break;
+        case 1: // Drivetrain running
+          break;
+        
+        case 2: // User/System pause
+          break;
 
-      /* Another sub-state for starting the test cycle will go here */
-      if(key == '*'){
-        mainState = 0;
-      }break;
-  }
+        case 3: // Emergenncy pause
+          break;
+      }break; // This } is for 'switch(testState)'
+      
+    case 2: /*** Testing menu ***/
+      break;
+    default:
+      setup();
+      break;
+  } // This } is for 'switch(mainState)'
 }
